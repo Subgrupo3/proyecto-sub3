73dcfe7c31b42c9fbc1676f4cc71242f2b1fe35a
camilasilva7 <camilasilvacabrera@gmail.com>
2023-11-01 09:20:15 -0300

    Punto 2 Entrega 7

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+const primerNombre = document.getElementById("inputPrimerNombre");
+const primerApellido = document.getElementById("inputPrimerApellido");
+const telefono = document.getElementById("inputTelefono");
+
+
+
+
+
 
 //Email
 const user = localStorage.getItem("userName"); //Obtengo el correo que se guardo en el localStorage cuando se ingresó
@@ -10,11 +18,19 @@ email.value = user; //Seteo el input de email para el correo que dio el usuario
 
 
 botonGuardarPerfil.addEventListener("click", function(){ //Lo que pasa cuando hace click en el boton "Guardar cambios"
+event.preventDefault();
 
-localStorage.setItem("userName", inputEmail.value); //Se setea el correo del usuario en el local storage con el ingresado en el input correo (Se actualiza el dropdown que muestra el username y el de los comentarios)
+//Verificamos campos obligatorios.
+if (primerNombre.value.trim() === '' || primerApellido.value.trim() === '' || email.value.trim() === '') {
+    alert ('Complete los campos obligatorios.');
 
-})
+} else {
+    localStorage.setItem("userName", inputEmail.value); //Se setea el correo del usuario en el local storage con el ingresado en el input correo (Se actualiza el dropdown que muestra el username y el de los comentarios)
+  alert ('Los cambios han sido guardados.')
+}
 
-})
+
+});
+});
 
 
